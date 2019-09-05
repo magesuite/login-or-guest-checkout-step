f205d33040ee8dc692fd31ab3653db6a88996643
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-09-05 18:11:02 +0200

    Fix styling and steps switching logic

--- view/frontend/web/js/view/login-or-guest.js
+++ view/frontend/web/js/view/login-or-guest.js
@@ -5,7 +5,7 @@ define([
     'Magento_Checkout/js/model/step-navigator',
     'jquery',
     'Magento_Customer/js/model/customer',
-], function (ko, Component, _, stepNavigator, $, customer) {
+], function(ko, Component, _, stepNavigator, $, customer) {
     'use strict';
 
     var $checkout = $('#checkout');
@@ -21,7 +21,7 @@ define([
          *
          * @returns {*}
          */
-        initialize: function () {
+        initialize: function() {
             this._super();
             // register your step
             stepNavigator.registerStep(
@@ -36,7 +36,7 @@ define([
             $checkout
                 .addClass('cs-login-or-guest')
                 .toggleClass('cs-login-or-guest--active', this.isVisible());
-            this.isVisible.subscribe(function (isVisible) {
+            this.isVisible.subscribe(function(isVisible) {
                 $checkout.toggleClass('cs-login-or-guest--active', isVisible);
             });
             // Move user directly to shipping step if they are logged in.
@@ -45,19 +45,25 @@ define([
             return this;
         },
 
-        navigate: function () { },
+        navigate: function() {
+            if (!customer.isLoggedIn()) {
+                this.isVisible(true);
+            }
+
+            this.navigateIfLoggedIn();
+        },
 
         /**
          * Moves user directly to shipping step if they are logged in.
          */
-        navigateIfLoggedIn: function () {
+        navigateIfLoggedIn: function() {
             var isLoggedInObservable = customer.isLoggedIn;
             if (isLoggedInObservable()) {
                 return this.navigateToShipping();
             }
 
             var isLoggedInSubscription = isLoggedInObservable.subscribe(
-                function (isLoggedIn) {
+                function(isLoggedIn) {
                     if (isLoggedIn) {
                         this.navigateToShipping();
                         isLoggedInSubscription.dispose();
@@ -70,32 +76,10 @@ define([
         /**
          * Waits for all checkout steps to load and navigates user to shipping.
          */
-        navigateToShipping: function () {
-            var stepsObservable = stepNavigator.steps;
-            var activeStep;
-            var nextStep;
+        navigateToShipping: function() {
             // Make sure that shipping step is already initialized before navigating to it.
-            if (stepsObservable().length > 2) {
-                // Set shipping step as not visible, otherwise shipping will be visible and next step will be payment
-                _.findWhere(steps, {code: 'shipping'}).isVisible(false);
-
-                stepNavigator.next();
-            } else {
-                var subscription = stepsObservable.subscribe(function (steps) {
-                    activeStep =
-                        steps[stepNavigator.getActiveItemIndex()] || {};
-                    nextStep = steps[1] || {};
-                    if (
-                        activeStep.code === 'login-or-guest' &&
-                        nextStep.code === 'shipping'
-                    ) {
-                        // Set shipping step as not visible, otherwise shipping will be visible and next step will be payment
-                        _.findWhere(steps, {code: 'shipping'}).isVisible(false);
-
-                        stepNavigator.next();
-                        subscription.dispose();
-                    }
-                });
+            if (window.location.hash.replace('#', '') === 'login-or-guest') {
+                stepNavigator.setHash('shipping');
             }
         },
     });
