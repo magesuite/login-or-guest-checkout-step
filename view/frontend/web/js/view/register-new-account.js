c4b5f588e2e3ae747d452d77f5622a6a0a2ee3d8
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2021-11-09 13:04:33 +0000

    feat: [IPET-88] Extend pre checkout with optional register section and guest email

---
+++ view/frontend/web/js/view/register-new-account.js
@@ -0,0 +1,27 @@
+define([
+    'ko',
+    'uiComponent',
+    'mage/url',
+    'jquery'
+], function(ko, Component, url, $) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'MageSuite_LoginOrGuestCheckoutStep/register-new-account',
+            registerUrl: url.build('customer/account/create'),
+        },
+        initialize: function(){
+            this._super();
+        },
+        /**
+         * If there is a content in static block with id login-page-create-account this content is copied and displayed and register section description
+         * If the is no such text registerText value is taken. It can be configured in xml
+        */
+        getRegisterText: function() {
+            var registerTextHtml = $('#register-text-container').html();
+            return registerTextHtml ? registerTextHtml : this.registerText;
+        }
+
+    });
+});
