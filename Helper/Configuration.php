783002c5a47be3240e30d0896afd1903463adb4d
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-11-09 13:27:08 +0100

    Added default values in config.xml

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -3,8 +3,8 @@ namespace MageSuite\LoginOrGuestCheckoutStep\Helper;
 
 class Configuration
 {
-    const XML_PATH_SHOW_REGISTER_SECTION = 'checkout/general/show_register_section';
-    const XML_PATH_SHOW_EMAIL_FIELD_FOR_GUEST = 'checkout/general/show_email_field_for_guest';
+    const XML_PATH_SHOW_REGISTER_SECTION = 'checkout/first_step/show_register_section';
+    const XML_PATH_SHOW_EMAIL_FIELD_FOR_GUEST = 'checkout/first_step/show_email_field_for_guest';
 
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
