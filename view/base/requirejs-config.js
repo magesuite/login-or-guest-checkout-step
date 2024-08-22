3e81f35d035dc81e78db135a156e78d9c0157b12
Dominik Kwaczynski <dominik.kwaczynski@creativestyle.pl>
2024-08-22 15:49:41 +0200

    fix [HAAR-4335] Fix issue with redirect to noroute for virtual quote on payment step on refresh

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -2,10 +2,7 @@ var config = {
     config: {
         mixins: {
             'Magento_Checkout/js/view/shipping': {
-                'MageSuite_LoginOrGuestCheckoutStep/js/view/shipping-payment-mixin': true,
-            },
-            'Magento_Checkout/js/view/payment': {
-                'MageSuite_LoginOrGuestCheckoutStep/js/view/shipping-payment-mixin': true,
+                'MageSuite_LoginOrGuestCheckoutStep/js/view/shipping-mixin': true,
             },
             'Magento_Checkout/js/model/step-navigator': {
                 'MageSuite_LoginOrGuestCheckoutStep/js/view/step-navigator-mixin': true,
